625799e8b91cccb70f6ddeca8b131d662e0ef32c
Carlos Henrique <carloshenrique3250@gmail.com>
2021-12-22 11:34:40 -0300

    Adicionando contatos, footer

--- main.js
+++ main.js
@@ -4,8 +4,31 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         } else {
             $('.navbar').removeClass("sticky");
+        } 
+        if (this.scrollY > 500) {
+            $('.scroll-up-btn').addClass("show");
+        } else {
+            $('.scroll-up-btn').removeClass("show");
         }
     });
+    $('.scroll-up-btn').click(function(){
+        $('html').animate({scrollTop: 0});
+    });
+
+    var typed = new Typed(".typing", {
+        strings:["Desenvolvedor Backend", "Desenvolvedor Frontend", "Analista", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
+    var typed = new Typed(".typing-2", {
+        strings:["Desenvolvedor", "Analista", "Freelancer"],
+        typeSpeed: 100,
+        backSpeed: 60,
+        loop: true
+    });
+
     $('.menu-btn').click(function(){
         $('.navbar .menu').toggleClass("active");
         $('.menu-btn i').toggleClass("active");
@@ -27,7 +50,7 @@ $(document).ready(function(){
             1000:{
                 items:3,
                 nav:false,
-            },
+            }
         }
     });
 });
