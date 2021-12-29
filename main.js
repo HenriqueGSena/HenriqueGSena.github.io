27fec3dbc85af4031b018041f8a24d7b83ec4866
Carlos Henrique <carloshenrique3250@gmail.com>
2021-12-29 12:53:08 -0300

    Alterando nomes

--- main.js
+++ main.js
@@ -16,14 +16,14 @@ $(document).ready(function() {
     });
 
     var typed = new Typed(".typing", {
-        strings:["Developer System", "Analista BI", "Freelancer"],
+        strings:["Software Engineer", "BI Analyst", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings:["Developer System", "Analista BI", "Freelancer"],
+        strings:["Software Engineer", "BI Analyst", "Freelancer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
